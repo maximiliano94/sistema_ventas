95682515b6b5634c53d3bed1490e449850728d9c
ReKeS <simon.mendoza06@gmail.com>
2017-11-13 13:12:57 -0300

    Packages

---
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/modelo/Usuario.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package cl.accenture.curso_java.sistema_ventas.modelo;
+
+/**
+ * @author Juan Mendoza
+ *
+ */
+public class Usuario {
+	
+	private String rut;
+	private String nombre;
+
+}
